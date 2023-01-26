e4798a22e39723b1ec5bb0415c30b22dd7b89d05
YeonjiKim0316 <73862452+YeonjiKim0316@users.noreply.github.com>
2023-01-26 18:00:36 +0900

    Update stock_info_app.py

--- stock_info_app.py
+++ stock_info_app.py
@@ -1,67 +1,66 @@
 import streamlit as st
 import pandas as pd
-import yfinance as yf
+import FinanceDataReader as fdr
 import datetime
 import matplotlib.pyplot as plt
 import matplotlib 
 from io import BytesIO
+import plotly.graph_objects as go
+import pandas as pd
 
-def get_stock_info(maket_type=None):
+def get_stock_info():
     base_url =  "http://kind.krx.co.kr/corpgeneral/corpList.do"    
     method = "download"
-    if maket_type == 'kospi':         
-        marketType = "stockMkt"      
-    elif maket_type == 'kosdaq':
-        marketType = "kosdaqMkt"    
-    elif maket_type == None:         
-        marketType = ""
-    url = "{0}?method={1}&marketType={2}".format(base_url, method, marketType)     
+    url = "{0}?method={1}".format(base_url, method)   
     df = pd.read_html(url, header=0)[0]
     df['종목코드']= df['종목코드'].apply(lambda x: f"{x:06d}")     
     df = df[['회사명','종목코드']]
     return df
 
-def get_ticker_symbol(company_name, maket_type):     
-    df = get_stock_info(maket_type)
+def get_ticker_symbol(company_name):     
+    df = get_stock_info()
     code = df[df['회사명']==company_name]['종목코드'].values    
-    code = code[0]
-    if maket_type == 'kospi':   
-        ticker_symbol = code +".KS"     
-    elif maket_type == 'kosdaq':      
-        ticker_symbol = code +".KQ" 
+    ticker_symbol = code[0]
     return ticker_symbol
-st.title("주식 정보를 가져오는 웹 앱") 
+
+st.title("무슨 주식을 사야 부자가 되려나...") 
 st.markdown(
     """     <style>
     [data-testid="stSidebar"][aria-expanded="true"] > div:first-child{width:250px;}     </style>
     """, unsafe_allow_html=True )
 
-st.sidebar.header("회사 이름과 기간 입력")
-stock_name = st.sidebar.text_input('회사 이름', value="NAVER") 
-date_range = st.sidebar.date_input("시작일과 종료일",
+st.sidebar.header("회사 이름과 기간을 입력하세요")
+stock_name = st.sidebar.text_input('회사 이름', value="삼성전자").upper()
+
+date_range = st.sidebar.date_input("시작일 - 종료일",
                  [datetime.date(2019, 1, 1), datetime.date(2021, 12, 31)]) 
-clicked = st.sidebar.button("주가 데이터 가져오기")
+clicked = st.sidebar.button("주가 데이터 확인")
+
 
 if(clicked == True):
-    ticker_symbol = get_ticker_symbol(stock_name, "kospi")     
-    ticker_data = yf.Ticker(ticker_symbol)
+    ticker_symbol = get_ticker_symbol(stock_name)     
     start_p = date_range[0]               
     end_p = date_range[1] + datetime.timedelta(days=1) 
-    df = ticker_data.history(start=start_p, end=end_p)
+    df = fdr.DataReader(ticker_symbol, start_p, end_p, exchange="KRX")
     df.index = df.index.date
     st.subheader(f"[{stock_name}] 주가 데이터")
-    st.dataframe(df.head())  
-    matplotlib.rcParams['font.family'] = 'Malgun Gothic'   
-    matplotlib.rcParams['axes.unicode_minus'] = False
-    ax = df['Close'].plot(grid=True, figsize=(15, 5)) 
-    ax.set_title("주가(종가) 그래프", fontsize=30)     
-    ax.set_xlabel("기간", fontsize=20)
-    ax.set_ylabel("주가(원)", fontsize=20)
-    plt.xticks(fontsize=15)
-    plt.yticks(fontsize=15)     
-    fig = ax.get_figure()  
-    st.pyplot(fig)
-    st.markdown("**주가 데이터 파일 다운로드**")     
+    st.dataframe(df.head())
+
+    # 캔들차트 만들기: https://plotly.com/python/candlestick-charts/
+    # Plotly에서 제목, x축 제목, y축 제목 등 변경: https://plotly.com/python/figure-labels/
+    st.subheader(f"[{stock_name}] 주가 그래프")
+    fig = go.Figure(data=go.Ohlc(x=df.index,
+                    open=df['Open'],
+                    high=df['High'],
+                    low=df['Low'],
+                    close=df['Close']))
+    fig.update_layout(
+        xaxis_title="기간",
+        yaxis_title="주가",
+        )
+
+    st.plotly_chart(fig, use_container_width=True)
+
     csv_data = df.to_csv()  
     excel_data = BytesIO()      
     df.to_excel(excel_data)     
